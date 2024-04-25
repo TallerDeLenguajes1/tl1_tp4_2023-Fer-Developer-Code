8bba13dd14a5dade71753c9ce84f3f5aafe92150
Victor Ibañez <ibanez.fernando.2018@gmail.com>
2024-04-25 18:33:21 -0300

    Probando

--- punto2.c
+++ punto2.c
@@ -15,30 +15,47 @@ typedef struct Tarea
 typedef struct NodoTarea
 {
     tarea nuevaTarea;
-    nodoTarea *siguiente;
+    struct NodoTarea *siguiente;
 } nodoTarea;
 
+void mostrarNodosPendientes(nodoTarea *start);
+void mostrarNodosRealizados(nodoTarea *start);
 nodoTarea *crearListaVacia();
 nodoTarea *crearTarea(int id);
-void InsertarNodo(nodoTarea **Start, nodoTarea *NuevoNodo);
-void insertarNodoAlFinal(nodoTarea ** start, nodoTarea *NuevoNodo);
-void eliminarNodo(nodoTarea ** start, int id);
+void insertarNodo(nodoTarea **Start, nodoTarea *NuevoNodo);
+void insertarNodoAlFinal(nodoTarea **start, nodoTarea *NuevoNodo);
+void eliminarNodo(nodoTarea **start, int id);
 
 int main()
 {
     nodoTarea *start = crearListaVacia();
-    int tareas, pendientes, realizadas;
-    printf("Cuantas tareas va a realizar");
-    scanf("%d", &tareas);
-    
+    int tarea, pendientes, realizadas, i = 0;
+    do
+    {
+        printf("Va a aniadir una tarea?\n1:Para si,0:Para no\n");
+        scanf(" %d", &tarea);
+        fflush(stdin);
+        if (tarea == 1)
+        {
+            nodoTarea *nuevo = crearTarea(i);
+            insertarNodo(&start, nuevo);
+            mostrarNodosPendientes(start);
+            i++;
+        }
+    } while (tarea != 0);
+
     return 0;
 }
 
 nodoTarea *crearTarea(int id)
 {
+    char buffer[MAX];
     nodoTarea *nuevaTarea = (nodoTarea *)malloc(sizeof(nodoTarea));
-    nuevaTarea->nuevaTarea.TareaID = id;
-    nuevaTarea->nuevaTarea.Descripcion = (char *)malloc(sizeof(char) * MAX);
+    nuevaTarea->nuevaTarea.TareaID = id + 1;
+    printf("Inserte la descripcion de la tarea\n");
+    fgets(buffer, MAX, stdin);
+    nuevaTarea->nuevaTarea.Descripcion = (char *)malloc(sizeof(char) * (strlen(buffer) + 1));
+    strcpy(nuevaTarea->nuevaTarea.Descripcion, buffer);
     nuevaTarea->nuevaTarea.Duracion = rand() % 91 + 10;
     nuevaTarea->siguiente = NULL;
     return nuevaTarea;
@@ -49,8 +66,21 @@ nodoTarea *crearListaVacia()
     return NULL;
 }
 
-void InsertarNodo(nodoTarea **Start, nodoTarea *NuevoNodo)
+void insertarNodo(nodoTarea **Start, nodoTarea *NuevoNodo)
 {
     NuevoNodo->siguiente = *Start;
     *Start = NuevoNodo;
+}
+
+void mostrarNodosPendientes(nodoTarea *start)
+{
+    printf("\t--------------------------------------------------------\n");
+    printf("\tID: %d\n", start->nuevaTarea.TareaID);
+    printf("\tDescripcion:\n \t\t%s", start->nuevaTarea.Descripcion);
+    printf("\tDuracion: %d\n", start->nuevaTarea.Duracion);
+    printf("\t--------------------------------------------------------\n");
+}
+
+void mostrarNodosRealizados(nodoTarea *start){
+    
 }
